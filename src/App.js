184e965ad4c41f4196d8ba8b6b82e63022124998
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-16 11:53:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+    {/* header */}
+    <header className="header">
+      <div className="logo">
+        <div className="play"></div>
+      </div>
+      <h1>My Movies</h1>
+    </header>
+    {/* navbar */}
+    <nav className="nav">
+      <ul>
+        <li><a href="/#">Start</a></li>
+        <li><a href="/#">Movies</a></li>
+        <li><a href="/#">Contact</a></li>
+      </ul>
+    </nav>
+    {/* main content */}
+    <section className="content">
+      {/* here the movies */}
+      <article className="movi-item">
+        <h3 className="title">Web development</h3>
+        <p className="description">lucafabbozzo.com</p>
+
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+      <article className="movi-item">
+        <h3 className="title">Web development</h3>
+        <p className="description">lucafabbozzo.com</p>
+
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+      <article className="movi-item">
+        <h3 className="title">Web development</h3>
+        <p className="description">lucafabbozzo.com</p>
+
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+            <article className="movi-item">
+        <h3 className="title">Web development</h3>
+        <p className="description">lucafabbozzo.com</p>
+
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </article>
+    </section>
+
+    {/* sidebar */}
+    <aside className="side">
+      <div className="search">
+        <h3 className="title">Searcher</h3>
+        <form>
+          <input type="text" />
+          <button>Search</button>
+        </form>
+      </div>
+
+      <div className="add">
+        <h3 className="title">Add Movie</h3>
+        <form>
+          <input type="text" placeholder="Title"/>
+          <textarea placeholder="description"></textarea>
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+      </aside>
+
+    {/* footer */}
+    <footer className="footer">
+      &copy; Master en React 
+    </footer>
+  
     </div>
-  );
+
+    );
 }
 
 export default App;
