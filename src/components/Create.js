b367906dbd33714649cae265d4675c5d1e8012c5
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-16 12:45:57 +0100

    create form for create movies

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Create = () => {
+
+  const titleComponent = "Add Movies";
+
+  const [movieState, setMovieState] = useState({
+    title: '',
+    description: ''
+  })
+
+  const { title, description } = movieState;
+
+  const getFormData = e => {
+    e.preventDefault()
+
+    //get data form
+    let target = e.target;
+    let title = target.title.value;
+    let description = target.description.value;
+
+    //Create object movie 
+
+    let movie = {
+      id: new Date().getTime(),
+      title,
+      description
+    };
+
+    setMovieState(movie)
+
+  }
+
   return (
     <div className="add">
-      <h3 className="title">Add Movie</h3>
-      <form>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="description"></textarea>
-        <input type="submit" value="Save" />
+      <h3 className="title">{titleComponent}</h3>
+      <strong>
+        {title && description && "You have create the movie: " + title}
+      </strong>
+
+      <form onSubmit={getFormData}>
+        <input type="text" id="title" name="title" placeholder="Title" />
+        <textarea
+          id="description"
+          name="description"
+          placeholder="description"
+        ></textarea>
+        <input type="submit" id="save" value="Save" />
       </form>
     </div>
   );
