bea17ca73b3f598100562e5311d5e39f58f9458c
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-25 08:58:47 +0000

    edit movie 2

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export const Edit = () => {
+
+  const title_component = "Edit Movie";
+
   return (
-    <div>Edit</div>
+    <div>
+      <h3 className='title'>{title_component}</h3>
+      <form className='edit_form'>
+        <input
+          type="text"
+          name="title"
+          className='edit_title'
+          defaultValue="Movie original title" />
+        <textarea
+          name='description'
+          defaultValue='Original description'
+          className='edit_description' />
+        <input type='submit' className='edit' value='Update' />
+      </form>
+    </div>
   )
 }
