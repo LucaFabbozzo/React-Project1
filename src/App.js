d578426f4af55a37d79000f61abebfd0be9abd81
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-26 11:13:00 +0200

    search with events, filters hooks

--- src/App.js
+++ src/App.js
@@ -10,41 +10,43 @@ function App() {
 
   return (
     <div className="layout">
-    {/* header */}
-    <header className="header">
-      <div className="logo">
-        <div className="play"></div>
-      </div>
-      <h1>My Movies</h1>
-    </header>
-    {/* navbar */}
-    <nav className="nav">
-      <ul>
-        <li><a href="/#">Start</a></li>
-        <li><a href="/#">Movies</a></li>
-        <li><a href="/#">Contact</a></li>
-      </ul>
-    </nav>
-    {/* main content */}
-    <section className="content">
-      {/* here the list movies */}
+      {/* header */}
+      <header className="header">
+        <div className="logo">
+          <div className="play"></div>
+        </div>
+        <h1>My Movies</h1>
+      </header>
+      {/* navbar */}
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/#">Start</a>
+          </li>
+          <li>
+            <a href="/#">Movies</a>
+          </li>
+          <li>
+            <a href="/#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      {/* main content */}
+      <section className="content">
+        {/* here the list movies */}
         <List listState={listState} setListState={setListState} />
-    </section>
+      </section>
 
-    {/* sidebar */}
-    <aside className="side">
-      <Searcher />
+      {/* sidebar */}
+      <aside className="side">
+        <Searcher listState={listState} setListState={setListState} />
         <Create setListState={setListState} />
-    </aside>
+      </aside>
 
-    {/* footer */}
-    <footer className="footer">
-      &copy; Master en React 
-    </footer>
-  
+      {/* footer */}
+      <footer className="footer">&copy; Master en React</footer>
     </div>
-
-    );
+  );
 }
 
 export default App;
--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Searcher = () => {
+export const Searcher = ({listState, setListState}) => {
+
+  const [search, setSearch] = useState('');
+
+  const searchMovie = (e) => {
+    //crear estado y actualizarlo
+    setSearch(e.target.value);
+
+    //filtrar para buscar coincidencias
+    let movie_finded = listState.filter(movie => {
+      return movie.title.toLowerCase().includes(search.toLocaleLowerCase());
+    })
+    
+    if (search.length <= 1 || movie_finded <= 0) {
+      movie_finded = JSON.parse(localStorage.getItem('movies'));
+    } 
+
+
+   
+
+    //actualizar estado del listado principal con lo que lografo filtrar
+    setListState(movie_finded)
+  }
+
   return (
     <div className="search">
-      <h3 className="title">Searcher</h3>
+      <h3 className="title">Searcher: {search}</h3>
       <form>
-        <input type="text" />
+        <input
+          type="text"
+          id='search_field'
+          name='search'
+          autoComplete='off'
+          value={search}
+          onChange={searchMovie}
+        />
         <button>Search</button>
       </form>
     </div>
