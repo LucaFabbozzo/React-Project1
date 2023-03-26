e584c481ee096137b98ae3c0b0af40be5aece7be
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-26 11:27:12 +0200

    message no-find

--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export const Searcher = ({listState, setListState}) => {
 
   const [search, setSearch] = useState('');
+  const [noFind, setNoFind] = useState(false);
 
   const searchMovie = (e) => {
     //crear estado y actualizarlo
@@ -11,11 +12,14 @@ export const Searcher = ({listState, setListState}) => {
     //filtrar para buscar coincidencias
     let movie_finded = listState.filter(movie => {
       return movie.title.toLowerCase().includes(search.toLocaleLowerCase());
-    })
+    });
     
     if (search.length <= 1 || movie_finded <= 0) {
       movie_finded = JSON.parse(localStorage.getItem('movies'));
-    } 
+      setNoFind(true);
+    } else {
+      setNoFind(false);
+    }
 
 
    
@@ -27,12 +31,17 @@ export const Searcher = ({listState, setListState}) => {
   return (
     <div className="search">
       <h3 className="title">Searcher: {search}</h3>
+      {(noFind === true && search.length >= 1) && (
+        <span className="no-find">
+          No se ha encontrado ninguna coincidencia
+        </span>
+      )}
       <form>
         <input
           type="text"
-          id='search_field'
-          name='search'
-          autoComplete='off'
+          id="search_field"
+          name="search"
+          autoComplete="off"
           value={search}
           onChange={searchMovie}
         />
